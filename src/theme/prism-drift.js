57841f2afcc49fe0cb758097a0e8d7deb8ad63cd
belicfr <96206878+belicfr@users.noreply.github.com>
2025-08-08 17:34:09 +0200

    feat: add support for hex and new keywords

--- src/theme/prism-drift.js
+++ src/theme/prism-drift.js
@@ -1,9 +1,10 @@
 Prism.languages.drift = {
   'comment': /\/\/.*/,
   'string': /"(?:\\.|[^\\"])*"/,
-  'keyword': /\b(?:if|else|while|return|fun|for|let|var|as)\b/,
-  'number': /\b\d+(?:\.\d+)?\b/,
-  'operator': /[+\-*\/=<>!.?@]+/,
+  'keyword': /\b(?:if|else|while|return|fun|for|let|var|as|repeat|empty|class|enum|prefab|interface|static|public|private|constructor|this)\b/,
+  'number': /\b0x[0-9a-fA-F]+\b|\b\d+\.\d+\b|\b\d+\b/
+  ,
+  'operator': /[+\-*\/=<>!?@]+|\.{1,2}/,
   'punctuation': /[{}[\];(),:]/,
   'identifier': /\b[a-zA-Z_][a-zA-Z0-9_]*\b/,
 };
