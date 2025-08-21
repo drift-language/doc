726b77a706a435ef88dfc21aaf1b351e23abe3f0
belicfr <96206878+belicfr@users.noreply.github.com>
2025-08-21 21:47:43 +0200

    feat: add many keywords and native implicit variables

--- src/theme/prism-drift.js
+++ src/theme/prism-drift.js
@@ -1,9 +1,8 @@
 Prism.languages.drift = {
   'comment': /\/\/.*/,
   'string': /"(?:\\.|[^\\"])*"/,
-  'keyword': /\b(?:if|else|while|return|fun|for|let|var|as|repeat|empty|class|enum|prefab|interface|static|public|private|constructor|this)\b/,
-  'number': /\b0x[0-9a-fA-F]+\b|\b\d+\.\d+\b|\b\d+\b/
-  ,
+  'keyword': /\b(?:if|else|while|return|fun|for|let|var|as|repeat|empty|class|enum|prefab|interface|static|public|protected|private|constructor|case|get|set|trigger|extend|with|implement|no|operator|subscript|override|final|throw|throws|catch|do|match)\b|\$super|\$this|try\?|try!|try/,
+  'number': /\b0x[0-9a-fA-F]+\b|\b\d+\.\d+\b|\b\d+\b/,
   'operator': /[+\-*\/=<>!?@]+|\.{1,2}/,
   'punctuation': /[{}[\];(),:]/,
   'identifier': /\b[a-zA-Z_][a-zA-Z0-9_]*\b/,
