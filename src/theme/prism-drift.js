029e8f16227a43cd69eb61f14b6f43619e53f11f
belicfr <96206878+belicfr@users.noreply.github.com>
2025-08-23 01:53:44 +0200

    feat: add many keywords

--- src/theme/prism-drift.js
+++ src/theme/prism-drift.js
@@ -1,7 +1,7 @@
 Prism.languages.drift = {
   'comment': /\/\/.*/,
   'string': /"(?:\\.|[^\\"])*"/,
-  'keyword': /\b(?:if|else|while|return|fun|for|let|var|as|repeat|empty|class|enum|prefab|interface|static|public|protected|private|constructor|case|get|set|trigger|extend|with|implement|no|operator|subscript|override|final|throw|throws|catch|do|match)\b|\$super|\$this|try\?|try!|try/,
+  'keyword': /\b(?:if|else|while|return|fun|for|let|var|as|repeat|empty|class|enum|prefab|interface|static|public|protected|private|constructor|case|get|set|trigger|extend|with|implement|no|operator|subscript|override|final|throw|throws|catch|do|match|defer|async|await|concurrent|parallel)\b|\$super|\$this|try\?|try!|try/,
   'number': /\b0x[0-9a-fA-F]+\b|\b\d+\.\d+\b|\b\d+\b/,
   'operator': /[+\-*\/=<>!?@]+|\.{1,2}/,
   'punctuation': /[{}[\];(),:]/,
